7b071978e753490125b9f34d20f24c72e3e9a1bc
Ahmed_Elbadawy <76036752+Ahmed55714@users.noreply.github.com>
2023-08-18 22:09:08 +0300

    Android Project

---
+++ app/src/main/java/eu/tutorials/a7_minutesworkoutapp/WorkOutApp.kt
@@ -0,0 +1,9 @@
+package eu.tutorials.a7_minutesworkoutapp
+
+import android.app.Application
+class WorkOutApp: Application() {
+
+    val db:HistoryDatabase by lazy {
+        HistoryDatabase.getInstance(this)
+    }
+}
